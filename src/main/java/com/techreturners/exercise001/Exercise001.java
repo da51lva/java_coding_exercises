c6fbd96c07447bd98d171e08c3ba606400438959
Matt DaSilva <matt_das@hotmail.co.uk>
2022-03-15 09:56:18 +0000

    Implement addVAT() in Ex001 and run relevant test

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -5,20 +5,22 @@ import java.util.List;
 public class Exercise001 {
 
     public String capitalizeWord(String word) {
-        String firstLetter = word.substring(0,1);
-        String remainingString = word.substring(1,word.length());
+        String firstLetter = word.substring(0, 1);
+        String remainingString = word.substring(1, word.length());
         return firstLetter.toUpperCase() + remainingString;
     }
 
     public String generateInitials(String firstName, String lastName) {
-        String firstInitial = firstName.substring(0,1).toUpperCase();
-        String lastInitial = lastName.substring(0,1).toUpperCase();
-        return firstInitial+"."+lastInitial;
+        String firstInitial = firstName.substring(0, 1).toUpperCase();
+        String lastInitial = lastName.substring(0, 1).toUpperCase();
+        return firstInitial + "." + lastInitial;
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double vatRateAsDecimal = 1 + vatRate / 100;
+        double newPrice = originalPrice * vatRateAsDecimal;
+        double newPriceRounded = Math.round(newPrice * 100.0) / 100.0; //rounds to 2 decimal places
+        return newPriceRounded;
     }
 
     public String reverse(String sentence) {
