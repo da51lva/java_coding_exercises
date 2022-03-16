eb8abbe2a314823aff76baa5de1439e496b39fc9
Matt DaSilva <matt_das@hotmail.co.uk>
2022-03-16 09:24:31 +0000

    Implement isPangram method in Ex005 and run Tests

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,31 @@
 package com.techreturners.exercise005;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Exercise005 {
 
+    private static final int NO_OF_LETTERS_IN_ALPHABET = 26;
+    private static final Pattern PATTERN = Pattern.compile("[a-z]");
+
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+
+        String lowerCaseInput = input.toLowerCase();
+        Matcher matcher = PATTERN.matcher(lowerCaseInput);
+        Set<String> matches = new HashSet<String>(); //stores unique matches
+
+        //add every match to Set to create unique matches
+        while (matcher.find()){
+            matches.add(matcher.group());
+        }
+
+        if (matches.size() == NO_OF_LETTERS_IN_ALPHABET)//checks every letter in alphabet was matched
+            return true;
+        else
+            return false;
+
     }
 
 }
